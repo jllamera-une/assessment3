a7c0583c4f6a7c7b2fd4092ab899b66991b06992
Jennica Llamera <jllamera@myune.edu.au>
2024-09-24 02:51:08 +1000

    Added create and edit page

--- src/pages/EditPost.tsx
+++ src/pages/EditPost.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 const EditPost = () => {
-  return <div>EditPost</div>;
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, 5, 6, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+  ];
+
+  return (
+    <section className="create-post">
+      <div className="container">
+        <h2>Edit Post</h2>
+        <p className="form__error-message">This is an error message</p>
+        <form className="form create-post__form">
+          <input
+            type="text"
+            placeholder="Title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            autoFocus
+          />
+          <ReactQuill
+            modules={modules}
+            formats={formats}
+            value={description}
+            onChange={setDescription}
+          />
+          <button type="submit" className="btn primary">
+            Edit
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default EditPost;
